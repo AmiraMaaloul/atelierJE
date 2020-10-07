a6655da01f48852168d18ab3ad9dd110e8946188
Amira Maaloul <amiramaaloul18@gmail.com>
2020-10-07 10:15:41 +0200

    Prepare network part

--- app/src/main/java/com/example/amira/atelierje/MainActivity.java
+++ app/src/main/java/com/example/amira/atelierje/MainActivity.java
@@ -9,6 +9,8 @@ import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceHolder.Callback;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -20,42 +22,54 @@ public class MainActivity extends AppCompatActivity implements Callback {
    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         mediaPlayer =  new MediaPlayer();
         SurfaceView surface = (SurfaceView)findViewById(R.id.surface);
         SurfaceHolder holder = surface.getHolder();
         holder.addCallback(this);
         holder.setFixedSize(400,300);
 
-        // Get a reference to the VideoView instance as follows, using the id we set in the XML layout.
-        VideoView vidView = (VideoView)findViewById(R.id.video);
+        Button playBtn = (Button) findViewById(R.id.btn_play);
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
+
+
+      /*
         // Add playback controls.
         MediaController vidControl = new MediaController(this);
         // Set it to use the VideoView instance as its anchor.
         vidControl.setAnchorView(vidView);
         // Set it as the media controller for the VideoView object.
         vidView.setMediaController(vidControl);
-
+*/
         // Prepare the URI for the endpoint.
         String vidAddress = "android.resource://" + getPackageName() + "/" + R.raw.video;
         Uri vidUri = Uri.parse(vidAddress);
         // Parse the address string as a URI so that we can pass it to the VideoView object.
-        vidView.setVideoURI(vidUri);
+       // vidView.setVideoURI(vidUri);
         // Start playback.
-        vidView.start();
+       // vidView.start();
     }
 
     public void surfaceCreated(SurfaceHolder holder)
     {
+        String vidAddress = "android.resource://" + getPackageName() + "/" + R.raw.video;
+
         try{
             mediaPlayer.setDisplay(holder);
-            mediaPlayer.setDataSource("/add the path");
+            mediaPlayer.setDataSource(vidAddress);
             mediaPlayer.prepare();
             mediaPlayer.start();
             int pos = mediaPlayer.getCurrentPosition();
             int duration = mediaPlayer.getDuration();
 
             mediaPlayer.seekTo(pos +(duration-pos)/10);
+
         }catch (IllegalArgumentException e)
         {
             Log.d("Media_player", e.getMessage());
@@ -63,7 +77,7 @@ public class MainActivity extends AppCompatActivity implements Callback {
         {
             Log.d("Media_player", e.getMessage());
         }
-        mediaPlayer.stop();
+
     }
 
     public void surfaceDestroyed(SurfaceHolder holder){
