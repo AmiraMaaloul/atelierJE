f36ae79a89ee5e6805d9c79317eba4e8238efeee
Amira Maaloul <amiramaaloul18@gmail.com>
2020-10-07 17:20:57 +0200

    Reading video from the net and control streaming

--- app/src/main/java/com/example/amira/atelierje/MainActivity.java
+++ app/src/main/java/com/example/amira/atelierje/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.amira.atelierje;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.app.Activity;
 import android.os.Bundle;
@@ -16,76 +20,110 @@ import android.widget.VideoView;
 
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity implements Callback {
+public class MainActivity extends AppCompatActivity implements Callback, MediaPlayer.OnPreparedListener {
     private MediaPlayer mediaPlayer;
-    @Override
-   public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    private SurfaceHolder videoHolder;
+    private SurfaceView videoSurface;
+    String vidAddress = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
 
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 
-        mediaPlayer =  new MediaPlayer();
-        SurfaceView surface = (SurfaceView)findViewById(R.id.surface);
-        SurfaceHolder holder = surface.getHolder();
-        holder.addCallback(this);
-        holder.setFixedSize(400,300);
-
-        Button playBtn = (Button) findViewById(R.id.btn_play);
-        playBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-
-      /*
-        // Add playback controls.
-        MediaController vidControl = new MediaController(this);
-        // Set it to use the VideoView instance as its anchor.
-        vidControl.setAnchorView(vidView);
-        // Set it as the media controller for the VideoView object.
-        vidView.setMediaController(vidControl);
-*/
-        // Prepare the URI for the endpoint.
-        String vidAddress = "android.resource://" + getPackageName() + "/" + R.raw.video;
-        Uri vidUri = Uri.parse(vidAddress);
-        // Parse the address string as a URI so that we can pass it to the VideoView object.
-       // vidView.setVideoURI(vidUri);
-        // Start playback.
-       // vidView.start();
     }
 
-    public void surfaceCreated(SurfaceHolder holder)
-    {
-        String vidAddress = "android.resource://" + getPackageName() + "/" + R.raw.video;
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void surfaceCreated(SurfaceHolder holder) {
 
-        try{
-            mediaPlayer.setDisplay(holder);
+        Uri vidUri = Uri.parse(vidAddress);
+        try {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDisplay(videoHolder);
             mediaPlayer.setDataSource(vidAddress);
             mediaPlayer.prepare();
-            mediaPlayer.start();
-            int pos = mediaPlayer.getCurrentPosition();
-            int duration = mediaPlayer.getDuration();
+            mediaPlayer.setOnPreparedListener(this);
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 
-            mediaPlayer.seekTo(pos +(duration-pos)/10);
-
-        }catch (IllegalArgumentException e)
-        {
+        } catch (IllegalArgumentException e) {
             Log.d("Media_player", e.getMessage());
-        }catch (IOException e)
-        {
+        } catch (IOException e) {
             Log.d("Media_player", e.getMessage());
         }
 
     }
 
-    public void surfaceDestroyed(SurfaceHolder holder){
+    public void surfaceDestroyed(SurfaceHolder holder) {
         mediaPlayer.release();
     }
 
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){
+    public void onPrepared(MediaPlayer media){
+        mediaPlayer.start();
+        /*int pos = mediaPlayer.getCurrentPosition();
+        int duration = mediaPlayer.getDuration();
+       mediaPlayer.seekTo(pos + (duration - pos) / 10);*/
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mediaPlayer = new MediaPlayer();
+        videoSurface = (SurfaceView) findViewById(R.id.surface);
+        videoHolder = videoSurface.getHolder();
+        videoHolder.addCallback(this);
+
+
+        MediaController videoControl = new MediaController(this);
+        videoControl.setAnchorView(videoSurface);
+       // videoSurface.setMediaController(videoControl);
+
+        videoHolder.setFixedSize(400, 300);
+
+
+        Button fastRewindBtn = (Button) findViewById(R.id.btn_fast_rewind);
+        fastRewindBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int pos = mediaPlayer.getCurrentPosition();
+                int duration = mediaPlayer.getDuration();
+                mediaPlayer.seekTo(pos - (duration - pos) / 10);
+            }
+        });
+
+        Button playBtn = (Button) findViewById(R.id.btn_play);
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            boolean stateVideo = false;
+            @Override
+            public void onClick (View v){
+                onPrepared(mediaPlayer);
+            }
+            /*if(stateVideo==false)
+            {
+                @Override
+                public void onClick (View v){
+                onPrepared(mediaPlayer);
+                }
+                stateVideo = true;
+            }
+            }else{
+                mediaPlayer.stop();
+            }*/
+        });
+
+        Button fastForwardBtn = (Button) findViewById(R.id.btn_fast_forward);
+        fastForwardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int pos = mediaPlayer.getCurrentPosition();
+                int duration = mediaPlayer.getDuration();
+                mediaPlayer.seekTo(pos + (duration - pos) / 5);
+            }
+        });
 
     }
 
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
 }
